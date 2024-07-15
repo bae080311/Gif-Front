16e1e989f8ccc33aec4c400639c6649e1c37aea7
bae080311 <156505831+bae080311@users.noreply.github.com>
2024-07-15 16:23:10 +0900

    feat:filterPopup

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
 const profileBtn = document.querySelector("#profile");
+const alert = document.querySelector("#alertPopup");
+
+document.querySelectorAll(".custom-dropdown .selected").forEach((dropdown) => {
+  dropdown.addEventListener("click", () => {
+    const options = dropdown.nextElementSibling;
+    options.style.display =
+      options.style.display === "block" ? "none" : "block";
+  });
+});
+
+document.querySelectorAll(".option").forEach((option) => {
+  option.addEventListener("click", () => {
+    const dropdown = option.closest(".custom-dropdown");
+    dropdown.querySelector(".selected").textContent = option.textContent;
+    dropdown.querySelector(".selected").dataset.value = option.dataset.value;
+    option.parentElement.style.display = "none";
+  });
+});
+
+document.addEventListener("click", (event) => {
+  const dropdowns = document.querySelectorAll(".custom-dropdown .options");
+  dropdowns.forEach((options) => {
+    if (!options.parentElement.contains(event.target)) {
+      options.style.display = "none";
+    }
+  });
+});
+
+function toggle() {
+  if (alert.className === "hidden") {
+    alert.classList.remove("hidden");
+  } else {
+    alert.className = "hidden";
+  }
+}
 
 profileBtn.addEventListener("click", () => {
   window.location.href = `/profile?id=${userId}`;
